ddebe5d268df3901f46f3dd8d73dee2cbe7f5eee
noobow34 <skywind.hk34@gmail.com>
2026-01-20 11:19:42 +0900

    プロジェクトファイルを追加。

---
+++ KenketsuNoAshiato/Controllers/RegisterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace KenketsuNoAshiato.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            string generatedId = GenerateId();
+            return View();
+        }
+
+        private string GenerateId(int length = 10)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            using var rng = RandomNumberGenerator.Create();
+            var bytes = new byte[length];
+            rng.GetBytes(bytes);
+            return new string(bytes.Select(b => chars[b % chars.Length]).ToArray());
+        }
+    }
+}
